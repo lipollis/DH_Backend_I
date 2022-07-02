bb0cf178a1bba8c5a3de034bda3342f433615e76
Aline Pollis <aline.pollis@hotmail.com>
2022-07-02 02:17:26 -0300

    Aula40-Integradora

---
+++ Aula40-Integradora/demo/src/main/java/com/example/demo/jwt/JwtUtil.java
@@ -0,0 +1,69 @@
+package com.example.demo.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    //chave secreta deste código
+    private String SECRET_KEY = "secret";
+
+    public String extractUserName(String token) {
+        return extractClaimUsername(token);
+    }
+
+    //extrai qdo expira
+    public Date extractExpiration(String token) {
+        return extractClaimDate(token);
+    }
+
+    //extrai a data
+    public Date extractClaimDate(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration();
+    }
+
+    public String extractClaimUsername(String token){
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                   //tempo de duração do token
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 +60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+
+
+
+}
