625bf6989a3abd51a1648e1637566ef0f95c8621
Aline Pollis <aline.pollis@hotmail.com>
2022-07-02 02:08:38 -0300

    Aula23-API Rest II

---
+++ Aula23-API Rest II/02_Aula-ClinicaOdonto/clinicaOdonto/src/main/java/com/example/clinicaOdonto/dto/EnderecoDTO.java
@@ -0,0 +1,27 @@
+package com.example.clinicaOdonto.dto;
+
+
+
+import com.example.clinicaOdonto.model.Endereco;
+import lombok.Getter;
+import lombok.Setter;
+
+
+// OBJETO SALVO NO BANCO
+
+// ASSINATURA LOMBOK
+@Getter
+@Setter
+public class EnderecoDTO {
+    // ATRIBUTOS
+    private Integer id;
+    private String rua;
+    private String numero;
+
+    // MÉTODO CONSTRUTOR - COMPARTILHAR AS INFORMAÇÕES - CONVERSÃO
+    public EnderecoDTO(Endereco endereco) {
+        this.id = endereco.getId();
+        this.rua = endereco.getRua();
+        this.numero = endereco.getNumero();
+    }
+}
